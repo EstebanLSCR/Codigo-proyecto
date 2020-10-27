1847ded39ff7afcddd60a05dc4cc91b70dfdfba2
MarcoME0407 <marco.mejiaelizondo@ucr.ac.cr>
2020-10-27 07:36:53 -0600

    datos

--- Proyecto.py
+++ Proyecto.py
@@ -3,7 +3,15 @@
 Created on Tue Oct 27 06:50:33 2020
 
 @author: Esteban
+@author: Marco
 """
 
 
 # Código proyecto de riesgo
+
+import pandas as pd
+import numpy as np
+import scipy
+import matplotlib.pyplot as plt
+
+datos = pd.read_excel("Defraudaciones enero-junio 2020.xlsx")
