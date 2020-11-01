c5973c2470899a088409e0ec2ccc4c061d524c2f
Felixrak <61946846+Felixrak@users.noreply.github.com>
2020-11-01 12:50:38 -0600

    Update Proyecto.py

--- Proyecto.py
+++ Proyecto.py
@@ -10,9 +10,61 @@ Created on Tue Oct 27 06:50:33 2020
 
 # Código proyecto de riesgo
 
+
+
+#%%
 import pandas as pd
 import numpy as np
-import scipy
+from scipy.stats import pareto
 import matplotlib.pyplot as plt
+from fitter import Fitter
+import seaborn as sns
+import statsmodels.api as sm
 
 datos = pd.read_excel("Defraudaciones enero-junio 2020.xlsx")
+
+
+#%%
+
+print(datos.columns)
+
+plt.hist(datos['MontoHistorico'])
+
+
+#%%
+f = Fitter(np.log(datos.MontoHistorico))
+
+f.fit()
+
+
+
+#%%
+
+
+f.summary()
+
+#fit = pareto.fit(datos.MontoHistorico)
+#print(fit)
+
+
+
+
+#%%
+logeados = np.log(datos.MontoHistorico)
+
+f = Fitter(logeados[logeados <= 14.65],  
+           distributions= ['pareto', 'gamma', 'dweibull','gennorm'])
+
+f.fit()
+
+f.summary()
+
+
+
+
+
+#%%
+
+
+
+
