1889bf51d8c1fdf043c08536f527a4294dbc9e0a
AMCA24 <73535476+AMCA24@users.noreply.github.com>
2020-11-01 09:38:29 -0600

    Update Proyecto.py

--- Proyecto.py
+++ Proyecto.py
@@ -3,7 +3,14 @@
 Created on Tue Oct 27 06:50:33 2020
 
 @author: Esteban
+@author: Amel
 """
 
 
 # Código proyecto de riesgo
+import numpy as np
+import scipy 
+import matplotlib.pyplot as plt
+import pandas as pd 
+
+pd.read_excel()
