f7e4625b852ae4574e31d47c7becbff97eaa9d69
Felixrak <61946846+Felixrak@users.noreply.github.com>
2020-11-05 22:35:57 -0600

    Update Proyecto.py

--- Proyecto.py
+++ Proyecto.py
@@ -19,6 +19,11 @@ from scipy import stats
 import matplotlib.pyplot as plt
 from fitter import Fitter
 import statsmodels.api as sm
+from datetime import datetime
+from datetime import timedelta
+from dfply import *
+from bioinfokit.analys import stat
+from scipy.stats import chisquare
 import datetime
 import seaborn as sns
 
@@ -35,8 +40,6 @@ plt.ylabel('Conteo')
 
 
 #%%
-
-## Ajustando para valores con la transformación logarítmica
 
 logeados = np.log(datos.MontoHistorico)
 
@@ -66,6 +69,41 @@ plt.xlabel('Reclamos transformación logarítmica')
 plt.ylabel('Densidad') 
 
 plt.savefig('Densidad.jpeg', format='jpeg', dpi=1300)
+
+
+#%% Frecuencias
+    #datos['mesO']=0
+    #datos['mesD']=0
+for i in range(0,len(datos.MontoHistorico)):
+    datos.mesO[i]=(datos.FechaOcurrencia[i]).month
+    datos.mesD[i]=(datos.FechaDescubrimiento[i]).month
+    
+#datos 
+#tipo: "O", "D"
+#...
+
+def F_frecuencias(datos):
+    #frec=[1,2,3,4,5,6,7,8,9,10,11,12]
+    frecO=datos["MontoHistorico"]
+    frecO=frecO[1:12]
+    #frecD=[1,2,3,4,5,6,7,8,9,10,11,12]
+    
+    
+    for i in range(1,13): 
+            frecO[i] = len((datos>>
+                               mask(X.mesO==i)).mesO)
+            
+            #frecD[i] = len((datos>>
+                            #mask(X.mesD==i)).mesD)
+            
+   # if(tipo=="O"):
+   #            frec = frecO
+                
+   # elif(tipo=="D"):
+   #             frec = frecd
+            
+                
+    return frecO
 
 
 #%%
@@ -212,6 +250,28 @@ fig.subplots_adjust(top=0.86)
 
 plt.show()
 
+#%%
+
+fechas = np.array([datetime.datetime.strptime(str(datos.FechaDescubrimiento[0]), 
+                               "%Y-%m-%d %H:%M:%S") ])
+for i in range(0, len(datos.FechaDescubrimiento)):
+    temp = datetime.datetime.strptime(str(datos.FechaDescubrimiento[i]), 
+                               "%Y-%m-%d %H:%M:%S")
+    fechas = np.append(fechas, temp)
+
+meses = [x.month for x in fechas]
+
+#df=datos.loc[:,["MontoHistorico","frecuenciaO"]]
+#df["BigFrec"]=df["frecuenciaO"]>30
+#observados=pd.crosstab(index=df['BigFrec'],columns=df['MontoHistorico'],margins=True)
+
+#res = stat()
+
+#res.chisq(df=datos)
+
+#Prueba chi cuadrado
+#vec=(F_frecuencias(datos,"O"))
+#chisquare(vec)
 
 #%%
 
@@ -223,6 +283,10 @@ f2 = Fitter(logeados2,
            distributions= ['gamma', 'dweibull',
                            'gennorm'])
 f2.fit()
+
+plt.title("Histograma de meses de descubrimiento")
+plt.xlabel('Mes')
+plt.ylabel('Conteo') 
 
 parametros_pareto2 = stats.genpareto.fit(logeados2, loc = 15 )
 
@@ -246,7 +310,6 @@ plt.ylabel('Densidad')
 plt.savefig('Densidad_cola.jpeg', format='jpeg', dpi=1300)
 
 plt.show()
-
 
 #%%
 
@@ -315,23 +378,7 @@ fig.subplots_adjust(top=0.86)
 
 plt.show()
 
-
-
-
-
 #%%
-
-
-
-
-
-
-
-
-
-
-#%%
-
 mes = ["enero", "febrero", "marzo", "abril", "mayo", "junio",
        "julio", "agosto", "setiembre", "octubre", "noviembre", "diciembre"]
 
@@ -368,7 +415,11 @@ alpha = 0.95
 
 def VaR_alpha(alpha, parametros):
     
-    if(parametros[0] == "normal"):
+    if(parametros[0] == "gennormal"):
+        from scipy.stats import gennorm
+        VaR = gennorm.ppf(alpha,parametros[1],parametros[2])
+        
+    elif(parametros[0] == "normal"):
         from scipy.stats import norm
         VaR = norm.ppf(alpha,parametros[1],parametros[2])
     
@@ -380,11 +431,18 @@ def VaR_alpha(alpha, parametros):
         from scipy.stats import pareto
         VaR = pareto.ppf(q=alpha,b=parametros[1],scale=parametros[2])
     
+    elif(parametros[0] == "weibull"):
+        from scipy.stats import weibull
+        VaR = weibull.ppf(q=alpha,b=parametros[1],scale=parametros[2])
     
-    
-    
+    else: #(parametros[0] == "lognorm"):
+        from scipy.stats import lognorm
+        VaR = lognorm.ppf(q=alpha,b=parametros[1],scale=parametros[2])
+        
     return VaR
     
 
-
-
+#%%Frecuencias
+for i in 1:len(datos.MontoHistorico):
+        
+    
